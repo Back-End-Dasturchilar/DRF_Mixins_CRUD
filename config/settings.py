2a7168504a1dbe1eb42b35c8ad7173728b1fdcb2
Asilbek <www.gubernatorrr@gmail.com>
2024-02-01 20:17:13 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # third-party-apps
     'rest_framework',
-
-    # local apps
     'blog',
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/library/', include('library.urls')),
+    path('api/main/path/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
